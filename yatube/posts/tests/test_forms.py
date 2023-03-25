2f075df2a53784f5158a12ee739fc243663b0825
Светлана Рябова <jisdtn@yandex.ru>
2023-03-26 00:01:37 +0100

    1

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -61,13 +61,13 @@ class PostFormTests(TestCase):
         """редактирует и сохраняет запись в Post."""
 
         small_gif = (
-             b'\x47\x49\x46\x38\x39\x61\x02\x00'
-             b'\x01\x00\x80\x00\x00\x00\x00\x00'
-             b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
-             b'\x00\x00\x00\x2C\x00\x00\x00\x00'
-             b'\x02\x00\x01\x00\x00\x02\x02\x0C'
-             b'\x0A\x00\x3B'
-        )
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B')
+
         uploaded = SimpleUploadedFile(
             name='small.gif',
             content=small_gif,
@@ -159,4 +159,3 @@ class CommentFormTests(TestCase):
         self.assertRedirects(
             response, (f'/auth/login/?next=/posts/{self.post.id}/comment/')
         )
-
