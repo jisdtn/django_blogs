50f923a558fc1217400e6b7a5ca3dcafb99e4ef1
Светлана Рябова <jisdtn@yandex.ru>
2023-03-27 21:38:35 +0200

    mihaershv

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -12,6 +12,8 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     '[::1]',
     'testserver',
+    'www.mihaershv.pythonanywhere.com',
+    'mihaershv.pythonanywhere.com',
 ]
 
 INSTALLED_APPS = [
@@ -26,6 +28,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "sorl.thumbnail",
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -36,7 +39,12 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+] 
 
 ROOT_URLCONF = "yatube.urls"
 
